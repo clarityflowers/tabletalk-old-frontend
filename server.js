e87bcf40a0b0a5f81977b20509f1aa50d29676a5
Cerisa <cerisa@strangerelics.com>
2016-09-18 16:39:03 -0700

    Setup basic express server

--- server.js
+++ server.js
@@ -1,9 +1,18 @@
-const http = require('http');
-const PORT=8080;
-function handleRequest(request, response) {
-  response.end('Test: ' + request.url);
+const express = require('express');
+let app = express();
+app.use(express.static('public'));
+
+const PORT = process.env.PORT
+if (!PORT) {
+  console.error('PORT vnvironment variable must be set');
+  console.log('Exiting');
+  return 1;
 }
-var server = http.createServer(handleRequest);
-server.listen(PORT, function() {
-  console.log("Server listening on : http://localhost:%s", PORT);
+
+app.get('/', function(req, res) {
+  res.send('hello world');
+});
+
+app.listen(PORT, function() {
+  console.log('Server started on port ' + PORT);
 })
