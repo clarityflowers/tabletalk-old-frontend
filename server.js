35fa2fbe75dc511123608e17397dcb04d74fec3f
Cerisa <cerisa@strangerelics.com>
2016-09-18 16:01:56 -0700

    Initial commit

---
+++ server.js
@@ -0,0 +1,9 @@
+const http = require('http');
+const PORT=8080;
+function handleRequest(request, response) {
+  response.end('Test: ' + request.url);
+}
+var server = http.createServer(handleRequest);
+server.listen(PORT, function() {
+  console.log("Server listening on : http://localhost:%s", PORT);
+})
