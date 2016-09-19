95e6885594d876f5f55a5f2cb8842daf0fe2b5d6
Cerisa <cerisa@strangerelics.com>
2016-09-18 17:48:43 -0700

    Fix build script

--- bin/start.sh
+++ bin/start.sh
@@ -1,5 +1,7 @@
-if [ "$NODE_ENV" = "production"]
+echo Initializing $NODE_ENV server
+if [ "$NODE_ENV" = "production" ]
 then
+  webpack
   node server.js
 else
   webpack-dev-server --content-base public/ --inline --colors --progress
