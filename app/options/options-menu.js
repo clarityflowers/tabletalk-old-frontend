aa77e002e0d9e77f067f1b33fbd1d6e365419f5f
Cerisa <cerisa@strangerelics.com>
2016-09-20 18:57:36 -0700

    Make options hovering shared between btn and label
    Now, when you hover over a button, the label is treated as hovered
    and vice versa. Click animations for the name label have been
    disabled for now as those will also need to be shared.

--- app/options/options-menu.js
+++ app/options/options-menu.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import update from 'react-addons-update';
 import { HoverWiggle } from 'utils/hover-animate.js';
 import './options-menu.scss';
 
@@ -7,10 +8,7 @@ class Label extends React.Component {
     super(props);
     this.state = {
       width: 0,
-      hover: 0,
       clicked: 0,
-      firstClickTimeout: null,
-      secondClickTimeout: null
     }
   }
   componentDidUpdate(prevProps, prevState) {
@@ -20,13 +18,10 @@ class Label extends React.Component {
     }
   }
   mouseEnter() {
-    this.setState({hover: 2});
-    setTimeout(() => {
-      this.setState({hover: this.state.hover - 1});
-    }, 150);
+    this.props.mouseEnter(this.props.name);
   }
   mouseLeave() {
-    this.setState({hover: this.state.hover - 1});
+    this.props.mouseLeave(this.props.name);
   }
   clickTimeout() {
     this.setState({
@@ -34,15 +29,15 @@ class Label extends React.Component {
     });
   }
   click() {
-    clearTimeout(this.state.firstClickTimeout);
-    clearTimeout(this.state.secondClickTimeout);
-    let firstClickTimeout = setTimeout(this.clickTimeout.bind(this), 150);
-    let secondClickTimeout = setTimeout(this.clickTimeout.bind(this), 1000);
-    this.setState({
-      clicked: 2,
-      firstClickTimeout: firstClickTimeout,
-      secondClickTimeout: secondClickTimeout
-    });
+    // clearTimeout(this.state.firstClickTimeout);
+    // clearTimeout(this.state.secondClickTimeout);
+    // let firstClickTimeout = setTimeout(this.clickTimeout.bind(this), 150);
+    // let secondClickTimeout = setTimeout(this.clickTimeout.bind(this), 1000);
+    // this.setState({
+    //   clicked: 2,
+    //   firstClickTimeout: firstClickTimeout,
+    //   secondClickTimeout: secondClickTimeout
+    // });
     this.props.onClick();
   }
   render() {
@@ -55,7 +50,7 @@ class Label extends React.Component {
       if (this.state.clicked > 1) {
         style.left = 10;
       }
-      else if (this.state.clicked == 0 && this.state.hover > 0) {
+      else if (this.state.clicked == 0 && this.props.isHovering > 1) {
         style.left = -10;
       }
     }
@@ -75,25 +70,49 @@ class Label extends React.Component {
 }
 
 const Labels = (props) => {
+  let nameHover = props.open ? props.isHovering.name : props.isHovering.toggle;
+  let nameName = props.open ? 'name' : 'toggle';
+  let nameOnClick = props.open ? props.goToUserSettings : props.toggle;
   return (
     <div className='labels'>
       <Label  hidden={!props.loggedIn || !props.name}
               text={props.name}
               id='name'
-              onClick={props.open ? props.goToUserSettings : props.toggle}/>
+              name={nameName}
+              onClick={nameOnClick}
+              isHovering={nameHover}
+              mouseEnter={props.mouseEnter}
+              mouseLeave={props.mouseLeave}/>
       <Label  hidden={!props.open}
               text='Sign off'
-              id='logout'
-              onClick={props.signout}/>
+              id='signout'
+              name='signout'
+              onClick={props.signout}
+              isHovering={props.isHovering.signout}
+              mouseEnter={props.mouseEnter}
+              mouseLeave={props.mouseLeave}/>
     </div>
   )
 }
 
 const OptionsButton = (props) => {
+  let mouseEnter = () => {
+    props.mouseEnter(props.name);
+  }
+  let mouseLeave = () => {
+    props.mouseLeave(props.name);
+  }
+  let className='';
+  if (props.isHovering) {
+    className='anim-wiggle';
+  }
   return (
-    <HoverWiggle>
-      <button onClick = {props.onClick}>{props.children}</button>
-    </HoverWiggle>
+    <button onClick={props.onClick}
+            className={className}
+            onMouseEnter={mouseEnter}
+            onMouseLeave={mouseLeave}>
+      {props.children}
+    </button>
   )
 }
 
@@ -106,10 +125,28 @@ const Divider = () => {
 const Banner = (props) => {
   return (
     <div className='banner'>
-      <OptionsButton onClick={props.toggle}>g</OptionsButton>
+      <OptionsButton  onClick={props.toggle}
+                      name="toggle"
+                      isHovering={props.isHovering.toggle}
+                      mouseEnter={props.mouseEnter}
+                      mouseLeave={props.mouseLeave}>
+        g
+      </OptionsButton>
       <Divider>g</Divider>
-      <OptionsButton onClick={props.signout}>p</OptionsButton>
-      <OptionsButton onClick={props.goToUserSettings}>u</OptionsButton>
+      <OptionsButton  onClick={props.signout}
+                      name="signout"
+                      isHovering={props.isHovering.signout}
+                      mouseEnter={props.mouseEnter}
+                      mouseLeave={props.mouseLeave}>
+        p
+      </OptionsButton>
+      <OptionsButton  onClick={props.goToUserSettings}
+                      name="name"
+                      isHovering={props.isHovering.name}
+                      mouseEnter={props.mouseEnter}
+                      mouseLeave={props.mouseLeave}>
+        u
+      </OptionsButton>
     </div>
   )
 }
@@ -118,20 +155,74 @@ class OptionsMenu extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      open: false
+      open: false,
+      isToggling: 0,
+      isHovering: {
+        name: 0,
+        signout: 0,
+        toggle: 0
+      },
+      timeouts: {
+        name: null,
+        signout: null,
+        toggle: null
+      }
+
     }
   }
   toggle() {
     this.setState({
-      open: !this.state.open
+      open: !this.state.open,
+      isToggling: 3
     });
+  }
+  hover(key) {
+    let isHovering = this.state.isHovering;
+    let timeouts = this.state.timeouts;
+    isHovering[key] = 3;
+    clearTimeout(timeouts[key]);
+    timeouts[key] = setTimeout(() => {
+      let timeouts = this.state.timeouts;
+      let isHovering = this.state.isHovering;
+      isHovering[key] = isHovering[key] - 1;
+      timeouts[key] = setTimeout(() => {
+        let isHovering = this.state.isHovering;
+        isHovering[key] = isHovering[key] - 1;
+        this.setState({isHovering: isHovering});
+      }, 600);
+      this.setState({
+        isHovering: isHovering,
+        timeouts: timeouts
+      });
+    }, 150);
+    this.setState({
+      isHovering: isHovering,
+      timeouts: timeouts
+    });
+  }
+  mouseEnter(key) {
+    let isHovering = this.state.isHovering;
+    if (isHovering[key]) {
+      isHovering[key] = 0;
+      this.setState({isHovering: isHovering});
+      setTimeout(() => {
+        this.hover(key);
+      }, 70);
+    }
+    else {
+      this.hover(key);
+    }
+  }
+  mouseLeave(key) {
+    let isHovering = this.state.isHovering;
+    isHovering[key] = isHovering[key] - 1;
+    this.setState({isHovering: isHovering});
   }
   signout() {
     this.setState({open: false});
     this.props.onSignOut();
   }
   goToUserSettings() {
-    // TODO
     this.toggle();
   }
   render() {
@@ -151,12 +242,19 @@ class OptionsMenu extends React.Component {
           loggedIn={this.props.loggedIn}
           open={this.state.open}
           signout={this.signout.bind(this)}
+          mouseEnter={this.mouseEnter.bind(this)}
+          mouseLeave={this.mouseLeave.bind(this)}
           toggle={this.toggle.bind(this)}
+          isHovering={this.state.isHovering}
+          isToggling={this.state.isToggling}
           goToUserSettings={this.goToUserSettings.bind(this)}
         />
         <Banner
           signout={this.signout.bind(this)}
           toggle={this.toggle.bind(this)}
+          mouseEnter={this.mouseEnter.bind(this)}
+          mouseLeave={this.mouseLeave.bind(this)}
+          isHovering={this.state.isHovering}
           goToUserSettings={this.goToUserSettings.bind(this)}
         />
       </div>
