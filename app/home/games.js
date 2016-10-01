d689cd99f288dd869cd5e276938d7141e0bb71b6
Cerisa <cerisa@strangerelics.com>
2016-09-30 22:57:35 -0700

    Replace swiping with transitiongroup
    I decided that the swiping animation looked clumsy, so now
    I'm just using the core enter/leave events by removing any games
    that aren't currently selected.

--- app/home/games.js
+++ app/home/games.js
@@ -93,14 +93,11 @@ let Plus = (props) => {
   )
 }
 
-let getGameClassName = (swipe, position) => {
+let getGameClassName = (position) => {
   return cx(
     'game',
     {
-      swipe: swipe != 0,
-      left: swipe < 0,
-      right: swipe > 0,
-      off: swipe <= -2 || swipe >= 2 || position == 0
+      off: position == 0
     }
   );
 }
@@ -127,9 +124,10 @@ class NewGame extends React.Component {
     this.timeout = setTimeout(resolve, duration);
   }
   newGameAnimation() {
-    console.log('NEW GAME ANIMATION: ' + this.state.newGameStatus);
-    let duration = NEW_GAME_ANIMATION_STEPS[this.state.newGameStatus];
-    console.log('duration=' + duration);
+    let duration = 0;
+    if (this.state.newGameStatus < NEW_GAME_ANIMATION_STEPS.length) {
+      duration = NEW_GAME_ANIMATION_STEPS[this.state.newGameStatus];
+    }
     if (duration) {
       this.setState({ newGameStatus: this.state.newGameStatus + 1 });
       this.setTimeout(this.newGameAnimation.bind(this), duration);
@@ -196,7 +194,7 @@ class NewGame extends React.Component {
       )
     }
     return (
-      <div className={getGameClassName(this.props.swipe, this.props.position)}>
+      <div className={getGameClassName(this.props.position)}>
         {content}
       </div>
     );
@@ -207,7 +205,7 @@ let OldGame = (props) => {
   let iconPosition = Math.min(props.position, 3);
   let boxPosition = Math.max(props.position - 3, 0);
   return (
-    <div className={getGameClassName(props.swipe, props.position)}>
+    <div className={getGameClassName(props.position)}>
       <GameIcon position={iconPosition}
                 entering={props.entering}
                 leaving={props.leaving}
@@ -218,6 +216,10 @@ let OldGame = (props) => {
     </div>
   );
 }
+
+const GAME_ANIMATION_STEPS_ENTERING = [ 700, 700, 700, 700 ]
+
+const GAME_ANIMATION_STEPS_EXITING =  [ 700, 700, 100, 100 ]
 
 class Game extends React.Component {
   constructor(props) {
@@ -232,38 +234,45 @@ class Game extends React.Component {
     this.leaveCallback = null;
     this.ready = true;
   }
-  componentDidUpdate(prevProps, prevState) {
-    if (!this.timeout) {
-      let duration = 300;
-      if (this.state.entering) {
-        if (this.state.position == 2 || this.state.position == 1) {
-          duration = 300;
-        }
-        this.timeout = setTimeout(() => {
-          this.timeout = null;
-          if (this.state.position < 4) {
-            this.setState({position: this.state.position + 1});
-          }
-          else if (this.enterCallback) {
-            this.enterCallback();
-            this.enterCallback = null;
-          }
-        }, duration);
+  setTimeout(resolve, duration) {
+    clearTimeout(this.timeout);
+    this.timeout = setTimeout(resolve, duration);
+  }
+  gameEnter() {
+    let duration = 0;
+    if (this.state.position < GAME_ANIMATION_STEPS_ENTERING.length) {
+      duration = GAME_ANIMATION_STEPS_ENTERING[this.state.position];
+    }
+    if (duration) {
+      this.setState({
+        position: this.state.position + 1,
+        entering: true
+      });
+      this.setTimeout(this.gameEnter.bind(this), duration);
+    }
+    else {
+      this.setState({
+        entering: false
+      })
+      if (this.enterCallback) {
+        this.enterCallback();
       }
-      else if (this.state.leaving) {
-        if (this.state.position >= 1) {
-          duration = 500;
-        }
-        this.timeout = setTimeout(() => {
-          this.timeout = null;
-          if (this.state.position > 0) {
-              this.setState({position: this.state.position - 1});
-          }
-          else if (this.leaveCallback) {
-            this.leaveCallback();
-          }
-        }, 500);
-      }
+    }
+  }
+  gameExit() {
+    let duration = 0;
+    if (this.state.position > 0) {
+      duration = GAME_ANIMATION_STEPS_EXITING[this.state.position - 1];
+    }
+    if (duration) {
+      this.setState({
+        position: this.state.position - 1,
+        leaving: true
+      });
+      this.setTimeout(this.gameExit.bind(this), duration);
+    }
+    else if (this.leaveCallback){
+      this.leaveCallback();
     }
   }
   componentWillUnmount() {
@@ -271,7 +280,7 @@ class Game extends React.Component {
   }
   componentWillEnter(callback) {
     this.enterCallback = callback;
-    this.setState({entering: true});
+    this.setTimeout(this.gameEnter.bind(this), 100);
   }
   componentDidEnter() {
     this.setState({
@@ -281,9 +290,7 @@ class Game extends React.Component {
   }
   componentWillLeave(callback) {
     this.leaveCallback = callback;
-    this.setState({
-      leaving: true
-    });
+    this.gameExit();
   }
   componentDidLeave() {
     this.props.doneLeaving();
@@ -294,8 +301,7 @@ class Game extends React.Component {
       content = (
         <NewGame position={this.state.position}
                  entering={this.state.entering}
-                 leaving={this.state.leaving}
-                 swipe={this.props.swipe}/>
+                 leaving={this.state.leaving}/>
       )
     }
     else {
@@ -304,8 +310,7 @@ class Game extends React.Component {
                  position={this.state.position}
                  entering={this.state.entering}
                  leaving={this.state.leaving}
-                 gameId={this.props.gameId}
-                 swipe={this.props.swipe}/>
+                 gameId={this.props.gameId}/>
       )
     }
     return content;
@@ -512,34 +517,28 @@ class Games extends React.Component {
       let game = this.state.games[i];
       let isTarget = this.props.target == game.id;
       let wasTarget = this.state.target == game.id;
-      let swipe = 0;
-      if (this.hasTarget(this.props.target) && !wasTarget) {
-        swipe = alternate;
-        alternate *= -1;
+      if (wasTarget || !this.hasTarget(this.props.target)) {
+        games.push(
+          <Game name={game.name}
+                key={game.id}
+                gameId={game.id}
+                doneEntering={this.onGameDoneEntering.bind(this)}
+                doneLeaving={this.onGameDoneLeaving.bind(this)}
+                isTarget={isTarget}>
+          </Game>
+        );
       }
-      games.push(
-        <Game name={game.name}
-              key={game.id}
-              gameId={game.id}
-              doneEntering={this.onGameDoneEntering.bind(this)}
-              doneLeaving={this.onGameDoneLeaving.bind(this)}
-              isTarget={isTarget}
-              swipe={swipe}>
-        </Game>
-      );
     }
     if (this.state.activeGames == this.state.games.length + 1) {
       let swipe = 0;
-      if (this.hasTarget(this.props.target)) {
-        swipe = alternate;
+      if (!this.hasTarget(this.props.target)) {
+        games.push(
+          <Game newGame
+          key='new'
+          doneEntering={this.onGameDoneEntering.bind(this)}
+          doneLeaving={this.onGameDoneLeaving.bind(this)}/>
+        );
       }
-      games.push(
-        <Game newGame
-              key='new'
-              doneEntering={this.onGameDoneEntering.bind(this)}
-              doneLeaving={this.onGameDoneLeaving.bind(this)}
-              swipe={swipe}/>
-      );
     }
     let backClass = cx(
       'back',
