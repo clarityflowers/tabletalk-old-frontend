f9bbaa36a94bd3ed80a850428d8b08dfc6a7c237
cchanna <cerisa@strangerelics.com>
2017-01-15 00:00:16 -0800

    Add cutter and slide items, rename guns
    Guns are named "blasters" in our hack, for now I'll just change the
    data until I implement hacking.

--- app/games/blades-in-the-dark/window/character/data/items.js
+++ app/games/blades-in-the-dark/window/character/data/items.js
@@ -10,16 +10,34 @@ const ITEMS = {
   "Climbing Gear": {
     load: 2
   },
-  "Concealed Palm Pistol": {
+  "Concealed Palm Blaster": {
     load: 0
   },
   "Demolition Tools": {
     load: 2
   },
+  "Fine Clothes & Jewelry": {
+    load: 0
+  },
   "Fine Cover Identity": {
     load: 0
   },
+  "Fine Heavy Weapon": {
+    load: 2
+  },
+  "Fine Loaded Dice, Trick Cards": {
+    load: 0
+  },
+  "Manacles & Chain": {
+    load: 0
+  },
+  "Rage Essence Vial": {
+    load: 0
+  },
   "Spiritbane Charm": {
+    load: 0
+  },
+  "Trance Powder": {
     load: 0
   },
   "Vial of Slumber Essence": {
@@ -28,7 +46,7 @@ const ITEMS = {
 }
 
 const COMMON_ITEMS = [
-  "A Blade or Two", "Throwing Knives", "A Pistol", "A 2nd Pistol",
+  "A Blade or Two", "Throwing Knives", "A Blaster", "A 2nd Blaster",
   "A Large Weapon", "An Unusual Weapon", "Armor", "+Heavy", "Burglary Gear",
   "Climbing Gear", "Arcane Implements", "Documents", "Subterfuge Supplies",
   "Demolition Tools", "Tinkering Tools"
@@ -37,7 +55,16 @@ const COMMON_ITEMS = [
 const PLAYBOOK_ITEMS = {
   "Spider": [
     "Fine Cover Identity", "Fine Bottle of Whisky", "Blueprints",
-    "Vial of Slumber Essence", "Concealed Palm Pistol", "Spiritbane Charm"
+    "Vial of Slumber Essence", "Concealed Palm Blaster", "Spiritbane Charm"
+  ],
+  "Cutter": [
+    "Fine Hand Weapon", "Fine Heavy Weapon", "Fine Scary Weapon or Tool",
+    "Manacles & Chain", "Rage Essence Vial", "Spiritbane Charm"
+  ],
+  "Slide": [
+    "Fine Clothes & Jewelry", "Fine Disguise Kit",
+    "Fine Loaded Dice, Trick Cards", "Trance Powder", "A Cane Sword",
+    "Spiritbane Charm"
   ]
 };
 
